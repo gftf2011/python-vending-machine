45819202bbdc5d523602811cad6931e5c06f61e4
gftf2011 <gftf2011@gmail.com>
2024-08-06 15:17:58 +0000

    test: add new tests

--- src/presentation/controllers/machine.py
+++ src/presentation/controllers/machine.py
@@ -5,6 +5,7 @@ from src.domain.contracts.dtos.base import BaseOutput
 from src.domain.contracts.dtos.machine import (
     ChooseProductOutputDTO,
     ChooseProductInputDTO,
+    AddCoinsInputDTO,
 )
 
 from src.domain.contracts.services.machine import IMachineService
@@ -17,12 +18,67 @@ class ChooseProductInputControllerDTO:
         self.product_code = product_code
 
 
+class AddCoinsInputControllerDTO:
+    def __init__(
+        self,
+        change: int,
+        coin_01_qty: int,
+        coin_05_qty: int,
+        coin_10_qty: int,
+        coin_25_qty: int,
+        coin_50_qty: int,
+        coin_100_qty: int,
+    ):
+        self.change = change
+        self.coin_01_qty = coin_01_qty
+        self.coin_05_qty = coin_05_qty
+        self.coin_10_qty = coin_10_qty
+        self.coin_25_qty = coin_25_qty
+        self.coin_50_qty = coin_50_qty
+        self.coin_100_qty = coin_100_qty
+
+
 class ChooseProductErrorOutputControllerDTO(BaseOutput):
-    def __init__(self, error: str):
-        self.error = error
+    def __init__(self, message: str):
+        self.message = message
 
     def to_dict(self) -> Any:
-        return {"error": self.error}
+        return {"error": {"message": self.message}}
+
+
+class AddCoinsErrorOutputControllerDTO(BaseOutput):
+    def __init__(
+        self,
+        message: str,
+        coin_01_qty: int,
+        coin_05_qty: int,
+        coin_10_qty: int,
+        coin_25_qty: int,
+        coin_50_qty: int,
+        coin_100_qty: int,
+    ):
+        self.message = message
+        self.coin_01_qty = coin_01_qty
+        self.coin_05_qty = coin_05_qty
+        self.coin_10_qty = coin_10_qty
+        self.coin_25_qty = coin_25_qty
+        self.coin_50_qty = coin_50_qty
+        self.coin_100_qty = coin_100_qty
+
+    def to_dict(self) -> Any:
+        return {
+            "error": {
+                "message": self.message,
+                "data": {
+                    "coin_01": self.coin_01_qty,
+                    "coin_05": self.coin_05_qty,
+                    "coin_10": self.coin_10_qty,
+                    "coin_25": self.coin_25_qty,
+                    "coin_50": self.coin_50_qty,
+                    "coin_100": self.coin_100_qty,
+                },
+            }
+        }
 
 
 class MachineController:
@@ -45,4 +101,32 @@ class MachineController:
         except Exception as error:
             return self._presenter.execute(
                 ChooseProductErrorOutputControllerDTO(str(error))
+            )
+
+    async def add_coins(self, input_dto: AddCoinsInputControllerDTO) -> Any:
+        try:
+            output = await self._machine_service.add_coins(
+                AddCoinsInputDTO(
+                    self._machine_id,
+                    input_dto.change,
+                    input_dto.coin_01_qty,
+                    input_dto.coin_05_qty,
+                    input_dto.coin_10_qty,
+                    input_dto.coin_25_qty,
+                    input_dto.coin_50_qty,
+                    input_dto.coin_100_qty,
+                )
+            )
+            return self._presenter.execute(output)
+        except Exception as error:
+            return self._presenter.execute(
+                AddCoinsErrorOutputControllerDTO(
+                    str(error),
+                    input_dto.coin_01_qty,
+                    input_dto.coin_05_qty,
+                    input_dto.coin_10_qty,
+                    input_dto.coin_25_qty,
+                    input_dto.coin_50_qty,
+                    input_dto.coin_100_qty,
+                )
             )
--- test/controllers/machine_test.py
+++ test/controllers/machine_test.py
@@ -7,6 +7,7 @@ from src.domain.entities.product import ProductEntity
 
 from src.presentation.controllers.machine import (
     ChooseProductInputControllerDTO,
+    AddCoinsInputControllerDTO,
     MachineController,
 )
 
@@ -38,7 +39,7 @@ class Test_Machine_Controller_Choose_Product:
         )
 
         assert (
-            json.loads(output)["error"]
+            json.loads(output)["error"]["message"]
             == 'machine - "'
             + "43c6fc3c-a51a-4c5d-9c1d-aae7e0c6ac4f"
             + '" - is not registered in the system'
@@ -93,7 +94,10 @@ class Test_Machine_Controller_Choose_Product:
             ChooseProductInputControllerDTO(products[0].code)
         )
 
-        assert json.loads(output)["error"] == "machine is not READY for operation"
+        assert (
+            json.loads(output)["error"]["message"]
+            == "machine is not READY for operation"
+        )
 
     @pytest.mark.asyncio
     async def test_should_return_error_message_if_product_does_not_exists(self):
@@ -144,7 +148,7 @@ class Test_Machine_Controller_Choose_Product:
             ChooseProductInputControllerDTO("03")
         )
 
-        assert json.loads(output)["error"] == "product does not exists"
+        assert json.loads(output)["error"]["message"] == "product does not exists"
 
     @pytest.mark.asyncio
     async def test_should_return_error_message_if_product_is_out_of_stock(self):
@@ -196,7 +200,7 @@ class Test_Machine_Controller_Choose_Product:
         )
 
         assert (
-            json.loads(output)["error"]
+            json.loads(output)["error"]["message"]
             == 'product - "'
             + products[0].id.value
             + '" - is not available in the machine'
@@ -256,3 +260,154 @@ class Test_Machine_Controller_Choose_Product:
         assert loaded_output["product_id"] == products[0].id.value
         assert loaded_output["product_price"] == products[0].unit_price
         assert loaded_output["product_name"] == products[0].name
+
+
+class Test_Machine_Controller_Add_coins:
+    @pytest.fixture(autouse=True)
+    def reset_fake_repos(self):
+        FakeMachineRepository.reset_instance()
+
+    @pytest.mark.asyncio
+    async def test_should_return_error_message_if_machine_is_not_registered(self):
+        machine_repo = FakeMachineRepository.get_instance()
+
+        machine_service = MachineService(machine_repo)
+
+        json_presenter = JSONPresenter()
+        machine_controller = MachineController(
+            json_presenter, machine_service, "43c6fc3c-a51a-4c5d-9c1d-aae7e0c6ac4f"
+        )
+
+        output = await machine_controller.add_coins(
+            AddCoinsInputControllerDTO(0, 0, 0, 0, 0, 0, 0)
+        )
+
+        assert (
+            json.loads(output)["error"]["message"]
+            == 'machine - "'
+            + "43c6fc3c-a51a-4c5d-9c1d-aae7e0c6ac4f"
+            + '" - is not registered in the system'
+        )
+        assert json.loads(output)["error"]["data"]["coin_01"] == 0
+        assert json.loads(output)["error"]["data"]["coin_05"] == 0
+        assert json.loads(output)["error"]["data"]["coin_10"] == 0
+        assert json.loads(output)["error"]["data"]["coin_25"] == 0
+        assert json.loads(output)["error"]["data"]["coin_50"] == 0
+        assert json.loads(output)["error"]["data"]["coin_100"] == 0
+
+    @pytest.mark.asyncio
+    async def test_should_return_error_message_if_machine_is_not_ready(self):
+        machine_id: str = "43c6fc3c-a51a-4c5d-9c1d-aae7e0c6ac4e"
+        products = [
+            ProductEntity.create(
+                "b9651752-6c44-4578-bdb6-883d703cbfff", "Hersheys", 1, "00", 100
+            ),
+            ProductEntity.create(
+                "b9651752-6c44-4578-bdb6-883d703cc000",
+                "Almond Joy Candy Bar",
+                1,
+                "01",
+                125,
+            ),
+            ProductEntity.create(
+                "b9651752-6c44-4578-bdb6-883d703cc001", "Twix Candy Bars", 1, "02", 75
+            ),
+        ]
+        owner = OwnerEntity.create(
+            "b9651752-6c44-4578-bdb6-883d703cbff5", "Sebastião Maia", "test@mail.com"
+        )
+        machine = MachineEntity.create(
+            machine_id,
+            owner,
+            MachineState.DISPENSING,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            products,
+        )
+
+        machine_repo = FakeMachineRepository.get_instance()
+
+        await machine_repo.save(machine)
+
+        machine_service = MachineService(machine_repo)
+
+        json_presenter = JSONPresenter()
+        machine_controller = MachineController(
+            json_presenter, machine_service, machine_id
+        )
+
+        output = await machine_controller.add_coins(
+            AddCoinsInputControllerDTO(0, 0, 0, 0, 0, 0, 0)
+        )
+
+        assert (
+            json.loads(output)["error"]["message"]
+            == "machine is not READY for operation"
+        )
+        assert json.loads(output)["error"]["data"]["coin_01"] == 0
+        assert json.loads(output)["error"]["data"]["coin_05"] == 0
+        assert json.loads(output)["error"]["data"]["coin_10"] == 0
+        assert json.loads(output)["error"]["data"]["coin_25"] == 0
+        assert json.loads(output)["error"]["data"]["coin_50"] == 0
+        assert json.loads(output)["error"]["data"]["coin_100"] == 0
+
+    @pytest.mark.asyncio
+    async def test_should_return_error_if_change_is_negative(self):
+        machine_id: str = "43c6fc3c-a51a-4c5d-9c1d-aae7e0c6ac4e"
+        products = [
+            ProductEntity.create(
+                "b9651752-6c44-4578-bdb6-883d703cbfff", "Hersheys", 1, "00", 100
+            ),
+            ProductEntity.create(
+                "b9651752-6c44-4578-bdb6-883d703cc000",
+                "Almond Joy Candy Bar",
+                1,
+                "01",
+                125,
+            ),
+            ProductEntity.create(
+                "b9651752-6c44-4578-bdb6-883d703cc001", "Twix Candy Bars", 1, "02", 75
+            ),
+        ]
+        owner = OwnerEntity.create(
+            "b9651752-6c44-4578-bdb6-883d703cbff5", "Sebastião Maia", "test@mail.com"
+        )
+        machine = MachineEntity.create(
+            machine_id,
+            owner,
+            MachineState.READY,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            products,
+        )
+
+        machine_repo = FakeMachineRepository.get_instance()
+
+        await machine_repo.save(machine)
+
+        machine_service = MachineService(machine_repo)
+
+        json_presenter = JSONPresenter()
+        machine_controller = MachineController(
+            json_presenter, machine_service, machine_id
+        )
+
+        output = await machine_controller.add_coins(
+            AddCoinsInputControllerDTO(-1, 1, 1, 1, 1, 1, 1)
+        )
+
+        assert json.loads(output)["error"]["message"] == "change can not be negative"
+        assert json.loads(output)["error"]["data"]["coin_01"] == 1
+        assert json.loads(output)["error"]["data"]["coin_05"] == 1
+        assert json.loads(output)["error"]["data"]["coin_10"] == 1
+        assert json.loads(output)["error"]["data"]["coin_25"] == 1
+        assert json.loads(output)["error"]["data"]["coin_50"] == 1
+        assert json.loads(output)["error"]["data"]["coin_100"] == 1
